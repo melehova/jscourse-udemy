a28b3a9c90db0b07e4793297e83ba246e2e7cdbb
veronika <veronika0melehova@gmail.com>
2022-12-14 14:02:46 +0200

    added: practice2

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-'use strict';
-//  Задание на урок:
+/* Задание на урок:
 
-// 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-// 'Сколько фильмов вы уже посмотрели?'
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
 const numberOfFilms = +prompt('How many movies have you already watched?', '');
 
-// 2) Создать объект personalMovieDB и в него поместить такие свойства:
-//     - count - сюда передается ответ на первый вопрос
-//     - movies - в это свойство поместить пустой объект
-//     - actors - тоже поместить пустой объект
-//     - genres - сюда поместить пустой массив
-//     - privat - в это свойство поместить boolean(логическое) значение false
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -19,19 +24,24 @@ const personalMovieDB = {
     privat: false,
 };
 
-// 3) Задайте пользователю по два раза вопросы:
-//     - 'Один из последних просмотренных фильмов?'
-//     - 'На сколько оцените его?'
 for (let i = 0; i < 2; i++) {
     const title = prompt('One of the last movies you watched?', ''),
-        rate = +prompt('How much would you rate it?', '');
-    personalMovieDB.movies[title] = rate;
+        rate = prompt('How much would you rate it?', '');
+    if (!title || !title.length || title.length >= 50 || !rate || !rate.length) {
+        i--;
+        continue;
+    }
+    personalMovieDB.movies[title] = +rate;
 }
-// Ответы стоит поместить в отдельные переменные
-// Записать ответы в объект movies в формате: 
-//     movies: {
-//         'logan': '8.1'
-//     }
 
-// Проверить, чтобы все работало без ошибок в консоли 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log('Watched quite a few films');
+} else if (personalMovieDB >= 10 && personalMovieDB.count < 30) {
+    console.log('You are a classic spectator');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Are you a cinephile');
+} else {
+    console.log('An error has occurred');
+}
