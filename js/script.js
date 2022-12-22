8423a3fdd384b14989796fd50ab22e2203d2a55a
veronika <veronika0melehova@gmail.com>
2022-12-22 09:03:58 +0200

    object, destructurizing

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 'use.strict';
 
-function fisrt() {
-    // do smth
-    setTimeout(function () {
-        console.log(1);
-    }, 500);
+const obj = new Object();
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red',
+    },
+    makeTest: function () {
+        console.log('test');
+    }
+};
+
+console.log(options.name);
+delete options.name;
+console.log(options);
+
+// get obj length
+let counter = 0;
+for (let key in options) {
+    counter++;
+    if (typeof options[key] === 'object') {
+        for (let i in options[key]) {
+            console.log(key, i, options[key][i]);
+        }
+    } else {
+        console.log(key, options[key]);
+    }
 }
+console.log(counter);
+// get obj length
+console.log(Object.keys(options).length);
+// obj methods
+options.makeTest();
+// destructurizing
+const {border, background} = options.colors;
 
-function second() {
-    console.log(2);
-}
-
-fisrt();
-second();
-
-function learnJs(lang, callback) {
-    console.log(`i learn: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log('i`ve learnt it');
-} 
-
-learnJs('jabascript', done);
+console.log(border);
