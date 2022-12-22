c6985eaad1bc67f0e659e6971fb2cac5d56a0853
veronika <veronika0melehova@gmail.com>
2022-12-22 08:43:30 +0200

    about callbacks

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use.strict';
+
+function fisrt() {
+    // do smth
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+fisrt();
+second();
+
+function learnJs(lang, callback) {
+    console.log(`i learn: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('i`ve learnt it');
+} 
+
+learnJs('jabascript', done);
