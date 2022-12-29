beeffdbfe3b0139b918a37fd582723e9423e6971
veronika <veronika0melehova@gmail.com>
2022-12-29 14:44:58 +0200

    Coding Exercise 11: Array Problems

--- js/script.js
+++ js/script.js
@@ -1,35 +1,18 @@
 'use.strict';
 
-const personalPlanPeter = {
-    name: "Peter",
-    age: "29",
-    skills: {
-        languages: ['ru', 'eng'],
-        programmingLangs: {
-            js: '20%',
-            php: '10%'
-        },
-        exp: '1 month'
-    },
-    showAgeAndLangs: (plan) => {
-        return `Мне ${plan.age} и я владею языками: ${plan.skills.languages.map(l => l.toUpperCase()).join(' ')}`;
-    },
-};
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
-function showExperience(plan) {
-    const {exp} = plan.skills;
-    return exp;
+function showFamily(arr) {
+    return arr.length ? `Семья состоит из: ${arr.join(' ')}` : 'Семья пуста';
 }
 
-function showProgrammingLangs(plan) {
-    let res = '';
-    for (let lang in plan.skills.programmingLangs) {
-        res += `Язык ${lang} изучен на ${plan.skills.programmingLangs[lang]}\n`;
-    }
-    return res;
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    arr.forEach(element => {
+        console.log(element.toLowerCase());
+    });
 }
 
-console.log(showExperience(personalPlanPeter));
-console.log(showProgrammingLangs(personalPlanPeter));
-
-console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+console.log(showFamily([]));
+standardizeStrings(favoriteCities);
