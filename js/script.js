21da48677ddcd4ac151ddf90d94c7933f024b82b
veronika <veronika0melehova@gmail.com>
2022-12-29 18:54:57 +0200

    Coding Exercise 14: (*) Advanced task for working with objects and arrays

--- js/script.js
+++ js/script.js
@@ -1,37 +1,20 @@
 'use.strict';
 
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-};
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
-function isBudgetEnough(data) {
-    const aria = data.shops.reduce((acum, cur) =>acum + cur.width*cur.length, 0);
-    const actualCost = aria*data.height*data.moneyPer1m3;
-    if (actualCost <= data.budget) {
-        return 'Бюджета достаточно';
-    } else {
-        return 'Бюджета недостаточно';
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const res = [];
+    for (let i = 0; i < arr.length; i += 3) {
+        res.push(arr.slice(i, i+3));
     }
+    const last = res[res.length-1];
+    if (last.length === 3) {
+        res.push('Оставшиеся студенты: -');
+    } else if (last.length < 3) {
+        res.splice(res.length-1, 1, `Оставшиеся студенты: ${last.join(', ')}`)
+    }
+    return res;
 }
 
-console.log(isBudgetEnough(shoppingMallData));
+console.log(sortStudentsByGroups(students));
