73e7d1006e21b84c08db8022b63c71177ec1c241
veronika <veronika0melehova@gmail.com>
2022-12-29 18:38:50 +0200

    Coding Exercise 13: (*) Advanced task for working with objects and arrays

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
 'use.strict';
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: () => {
-        console.log('Hello');
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    const aria = data.shops.reduce((acum, cur) =>acum + cur.width*cur.length, 0);
+    const actualCost = aria*data.height*data.moneyPer1m3;
+    if (actualCost <= data.budget) {
+        return 'Бюджета достаточно';
+    } else {
+        return 'Бюджета недостаточно';
     }
-};
+}
 
-const john0 = {
-    health: 100,
-};
-// old format deprecated!
-// john.__proto__ = soldier;
-
-//                    to     from
-Object.setPrototypeOf(john0, soldier);
-
-const john = Object.create(soldier);
-console.log(john);
-john.sayHello();
-
+console.log(isBudgetEnough(shoppingMallData));
