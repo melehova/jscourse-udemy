ede0f655b4ceddfe62762d5abc352ee7020d2f31
veronika <veronika0melehova@gmail.com>
2022-12-29 14:36:49 +0200

    Coding Exercise 10: Tasks for working with objects

--- js/script.js
+++ js/script.js
@@ -1,93 +1,35 @@
 'use.strict';
 
-// let a = 5,
-//     b = a;
-
-// b += 5;
-// console.log(b);
-// console.log(a);
-
-// const obj = {
-//     a: 5,
-//     b: 1
-// };
-
-// const copy = obj; // посилання
-// copy.a = 10; // модифікуємо вихідний об'єкт
-
-// console.log(copy, obj);
-
-function copy(mainObj) {
-    let objCopy = {};
-    for (let key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4,
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: (plan) => {
+        return `Мне ${plan.age} и я владею языками: ${plan.skills.languages.map(l => l.toUpperCase()).join(' ')}`;
     },
 };
 
-const newNumbers = copy(numbers);
-newNumbers.c.x = 8;
-newNumbers.a = 8;
-// console.log(newNumbers, numbers);
-
-const add = {
-    d: 17,
-    e: 20,
-    k: {
-        x: 7,
-        y: 4,
-    },
-};
-const clone = Object.assign({}, add);
-clone.d = 21;
-clone.k.x = 10;
-// console.log(clone);
-// console.log(add);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'okeeey';
-
-// console.log(newArray);
-// console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'okey'],
-    blogs = ['wordpress', 'liejournal', 'blogger'],
-    internet = [...video, ...blogs, 'insta'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
 }
 
-const num = [2,5,8];
-
-log(...num);
-
-const array = ['a', 'b'];
-const newArray1 = [...array];
-
-const q = {
-    one: 1,
-    two: 2,
-    three: {
-        a: 5,
+function showProgrammingLangs(plan) {
+    let res = '';
+    for (let lang in plan.skills.programmingLangs) {
+        res += `Язык ${lang} изучен на ${plan.skills.programmingLangs[lang]}\n`;
     }
-};
+    return res;
+}
 
-const newObj = {...q};
+console.log(showExperience(personalPlanPeter));
+console.log(showProgrammingLangs(personalPlanPeter));
 
-console.log(q, newObj);
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
