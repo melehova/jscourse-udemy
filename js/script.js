2f9c1c9e83a4f5536e453fdb94e78916ca88e30d
veronika <veronika0melehova@gmail.com>
2023-01-27 23:28:47 +0200

    43. Practice. Task for working out actions with the page reviewed

--- js/script.js
+++ js/script.js
@@ -33,10 +33,6 @@ document.querySelector('.promo__bg').style.backgroundImage = 'url(../img/bg.jpg)
 // 4
 const ul = document.querySelector('ul.promo__interactive-list');
 ul.querySelectorAll('li').forEach(li => li.remove());
-const ol = document.createElement('ol');
-ol.style.listStyleType = 'decimal';
-ul.classList.forEach(clas => ol.classList.add(clas));
-ul.replaceWith(ol);
 movieDB.movies.sort().forEach((movie, idx) => {
     const li = document.createElement('li');
     li.classList.add('promo__interactive-item');
@@ -45,5 +41,5 @@ movieDB.movies.sort().forEach((movie, idx) => {
     const div = document.createElement('div');
     div.classList.add('delete');
     li.append(div);
-    ol.append(li);
+    ul.append(li);
 });
