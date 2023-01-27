2720ad68c22b16d94c8cabc973a692acd9c04eae
veronika <veronika0melehova@gmail.com>
2023-01-27 17:08:06 +0200

    Tasks for finding errors in the code SOLUTION

--- js/script.js
+++ js/script.js
@@ -2,54 +2,51 @@
 
 const restorantData = {
   menu: [
-      {
-          name: 'Salad Caesar',
-          price: '14$'
-      },
-      {
-          name: 'Pizza Diavola',
-          price: '9$'
-      },
-      {
-          name: 'Beefsteak',
-          price: '17$'
-      },
-      {
-          name: 'Napoleon',
-          price: '7$'
-      }
+    {
+      name: 'Salad Caesar',
+      price: '14$'
+    },
+    {
+      name: 'Pizza Diavola',
+      price: '9$'
+    },
+    {
+      name: 'Beefsteak',
+      price: '17$'
+    },
+    {
+      name: 'Napoleon',
+      price: '7$'
+    }
   ],
   waitors: [
-      {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    { name: 'Alice', age: 22 }, { name: 'John', age: 24 }
   ],
   averageLunchPrice: '20$',
   openNow: true
 };
 
-function isOpen(prop) {
-  let answer = '';
-  prop ? answer = 'Закрыто' : answer = 'Открыто';
+const isOpen = prop => prop ? 'Открыто' : 'Закрыто';
 
-  return anwser;
-}
-
-console.log(isOpen(openNow))
+console.log(isOpen(restorantData.openNow));
 
 function isAverageLunchPriceTrue(fDish, sDish, average) {
-  if (+fDish.price.slice(0, -1) + (sDish.price) < average) {
-      return 'Цена ниже средней';
+  if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < average) {
+    return 'Цена ниже средней';
   } else {
-      return 'Цена выше средней';
+    return 'Цена выше средней';
   }
 }
 
 console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
 
 function transferWaitors(data) {
-  const copy = Object.assign({}, data);
+  const copy = JSON.parse(JSON.stringify(data));
 
-  copy.waitors[0] = {name: 'Mike', age: 32};
+  copy.waitors[0] = { name: 'Mike', age: 32 };
   return copy;
 }
 
-transferWaitors(restorantData);
+let newRest = transferWaitors(restorantData);
+console.log(restorantData);
+console.log(newRest);
